c8f548c4362d72761b76d94fc7da87d3b56c63c5
Xurshid <kaxudi34801@gmail.com>
2023-02-20 02:29:58 +0500

    choose cars tab-menu some bugs fixed

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ let darkSVG = mode.querySelector("#darkSVG");
 let lightSVG = mode.querySelector("#lightSVG");
 let tabList = document.querySelector('.choose-car__type-tab');
 let tabListElements = Array.from(tabList.children);
+let activeElementIndex = 0;
 
 removeAddClass(tabListElements[0]);
 
@@ -36,6 +37,10 @@ else if (localStorage.theme === 'dark') {
   html.classList.add('dark');
   darkSVG.classList.add("hidden");
   lightSVG.classList.toggle("hidden");
+  tabListElements.slice(1).forEach(element => {
+    element.classList.remove('tab-element-light-default');
+    element.classList.add('tab-element-dark-default');
+  })
 }
 
 mode.addEventListener("click", (event) => {
@@ -44,11 +49,23 @@ mode.addEventListener("click", (event) => {
     localStorage.setItem("theme", "dark");
     darkSVG.classList.add("hidden");
     lightSVG.classList.toggle("hidden");
+    tabListElements.forEach((element, index) => {
+      if(index !== activeElementIndex){
+         tabListElements[index].classList.remove('tab-element-light-default');
+         tabListElements[index].classList.add('tab-element-dark-default');
+      }
+    })
   }
   else {
     localStorage.setItem("theme", "light");
     lightSVG.classList.add("hidden");
     darkSVG.classList.remove("hidden");
+    tabListElements.forEach((element, index) => {
+      if(index !== activeElementIndex){
+         tabListElements[index].classList.remove('tab-element-dark-default');
+         tabListElements[index].classList.add('tab-element-light-default');
+      }
+    })
   }
 })
 //
@@ -57,12 +74,17 @@ mode.addEventListener("click", (event) => {
 
 function removeAddClass(element, reverse = false){
   if(!reverse){
-    element.classList.remove('bg-white', 'text-black');
+    element.classList.remove('tab-element-light-default', 'tab-element-dark-default');
     element.classList.add('tab-active-element');
   }
   else{
     element.classList.remove('tab-active-element');
-    element.classList.add('bg-white', 'text-black');
+    if(localStorage.theme === 'dark'){
+      element.classList.add('tab-element-dark-default');
+    }
+    else{
+      element.classList.add('tab-element-light-default');
+    }
   }
 }
 
@@ -74,6 +96,7 @@ tabListElements.forEach((element, index, array) => {
   element.addEventListener("click", (event) => {
     //get key value from element
     let keyAttributeValue = +event.target.getAttribute('key');
+    activeElementIndex = keyAttributeValue;
     array.forEach((elementPassive, indexPassive) => {
       if(indexPassive !== keyAttributeValue){
         removeAddClass(tabListElements[indexPassive], true); 
